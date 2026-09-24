9d1237b7bdb98e3ed04e1954f83c97ce780c53e6
Sakura <xuerdml@163.com>
2026-09-24 16:41:03 +0800

    调整音量、更新时间

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -12,7 +12,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	mode: "local",
 
 	// 默认音量 (0-1)
-	volume: 0.7,
+	volume: 0.5,
 
 	// 播放模式：'list'=列表循环, 'one'=单曲循环, 'random'=随机播放
 	playMode: "list",
